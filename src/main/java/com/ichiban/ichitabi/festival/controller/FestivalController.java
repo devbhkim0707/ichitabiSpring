b3f8297fcf2286b50b253ad16ade68181bee7075
devbhkim0707 <devbhkim0707@gmail.com>
2026-01-13 14:09:35 +0900

    feat) festival mapper get method for index page festival carousel

--- src/main/java/com/ichiban/ichitabi/festival/controller/FestivalController.java
+++ src/main/java/com/ichiban/ichitabi/festival/controller/FestivalController.java
@@ -1,11 +1,34 @@
 package com.ichiban.ichitabi.festival.controller;
 
+import com.ichiban.ichitabi.festival.dto.FestivalDto;
+import com.ichiban.ichitabi.festival.service.FestivalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/festival")
 public class FestivalController {
 
+    @Autowired
+    private FestivalService festivalService;
 
+    @GetMapping("/")
+    public String festivalPage(
+            @RequestParam(defaultValue = "ALL") String season,
+            Model model
+    ) {
+        List<FestivalDto> festivalItems =
+                festivalService.selectFestivalList(season);
+
+        model.addAttribute("festivalItems", festivalItems);
+        model.addAttribute("currentSeason", season);
+
+        return "fragment/festival_carousel::festivalCarousel";
+    }
 }
--- src/main/java/com/ichiban/ichitabi/festival/service/FestivalService.java
+++ src/main/java/com/ichiban/ichitabi/festival/service/FestivalService.java
@@ -1,9 +1,21 @@
 package com.ichiban.ichitabi.festival.service;
 
+import com.ichiban.ichitabi.festival.dto.FestivalDto;
+import com.ichiban.ichitabi.festival.mapper.FestivalMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FestivalService {
 
+    @Autowired
+    private FestivalMapper festivalMapper;
+
+    public List<FestivalDto> selectFestivalList(String season) {
+
+        return festivalMapper.selectFestivalList(season);
+    }
 
 }
